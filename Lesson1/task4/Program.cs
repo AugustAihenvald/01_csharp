e29b7ead26538ba7884772a96b80d53b24183b85
Anton <augustaihenvald@gmail.com>
2024-04-16 23:41:37 +0300

    Максимум из 5 чисел

---
+++ Lesson1/task4/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 5;
+int b = 6;
+int c = 92;
+int d = 54;
+int e = -5;
+int max = a;
+
+if (b>max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;
+}
+if (d>max)
+{
+    max = d;
+}
+if (e>max)
+{
+    max = e;
+}
+Console.WriteLine(max);
