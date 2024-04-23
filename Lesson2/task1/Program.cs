71c6443376fa532c4d0ab98f7efd4386afa5c784
Anton <augustaihenvald@gmail.com>
2024-04-23 08:14:53 +0300

    ывод первого и последнего числа трехначного числа

---
+++ Lesson2/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.Diagnostics;
+
+Console.WriteLine("Ведите трехзначное число");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+if (Num>=100 && Num<=999)
+{
+    int firstDigit = Num / 100;
+    int thirdDigit = Num % 10;
+
+    int result = firstDigit*10+ thirdDigit;
+
+    Console.WriteLine(result);
+
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
