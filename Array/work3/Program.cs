09bdbdff1c91eaaf2fac2c59ea95677ae37b265e
Anton <augustaihenvald@gmail.com>
2024-04-24 16:40:02 +0300

    Add work3

---
+++ Array/work3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+int n =10;
+int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int i = 0;
+
+while(i<n)
+{
+    if (arr[i]%2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    } 
+    i = i+1;
+}
+
