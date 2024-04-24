d764abfb5971b4c07acf76f050315b60599baf6f
Anton <augustaihenvald@gmail.com>
2024-04-24 17:00:26 +0300

    add giri

--- Array/work4/Program.cs
+++ Array/work4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n = 5;
+int[] array = {1,2,3,6,8};
+int i = 0;
+int max = array[0];
+while(i<n)
+{
+    if (array[i]>max)
+    {
+        max = array[i];
+    }
+    i = i +1;
+}
+Console.WriteLine(max);
