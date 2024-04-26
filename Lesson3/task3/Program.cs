00d49a9c4ea4bd4c52e6abc72bb3230d91b1dd4a
Anton <augustaihenvald@gmail.com>
2024-04-26 23:37:35 +0300

    Task3

---
+++ Lesson3/task3/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 3, 2, 4, 2, 3 };
+int[] array2 = new int[array.Length / 2];
+
+int left = 0;
+int right = array.Length - 1;
+
+while (left < right)
+{
+    array2[left] = array[left] * array[right];
+    left++;
+    right--;
+}
+
+for (int i = 0; i < array2.Length; i++)
+{
+    Console.Write(array2[i] + " ");
+}
