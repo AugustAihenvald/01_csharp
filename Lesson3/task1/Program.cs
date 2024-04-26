19d7bdd5117468fc27b10403cfb6776b6956f5f5
Anton <augustaihenvald@gmail.com>
2024-04-26 22:23:59 +0300

    Тернарные операторы

--- Lesson3/task1/Program.cs
+++ Lesson3/task1/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿int[] array = { 1, 3, 4, 19, 3 };
+int num = 4;
+bool isExistNum = false;
+
+for (int i =0; i < array.Length; i++)
+{
+    if (array[i] == num)
+    {
+        isExistNum = true;
+        break;
+
+    }
+}
+string result = isExistNum ? "Да": "Нет";
+Console.WriteLine(result);
