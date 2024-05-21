51ce2392584f732b1659f56031f9296375f77beb
Anton <augustaihenvald@gmail.com>
2024-05-21 20:37:01 +0300

    finish tasks

---
+++ lesson6/task2/Program.cs
@@ -0,0 +1,39 @@
+﻿/* На основе символов строки (тип string) сформировать массив
+символов (тип char[]). Вывести массив на экран.
+Указание
+Метод строки ToCharArray() не использовать.
+Пример
+“Hello!” => [‘H’, ‘e’, ‘l’, ‘l’, ‘o’, ‘!’ ] */
+
+using System.Reflection;
+
+char[] CreateCharToString(string str)
+{
+    char[] arr = new char[str.Length];
+    
+    for (int i = 0; i < str.Length; i++)
+    {
+       arr[i]= str[i]; 
+    }
+    return arr;
+}
+
+void PrintArr(char[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+       if (i<arr.Length-1)
+       {
+            Console.Write($"'{arr[i]}', ");
+       }
+       else
+       {
+        Console.Write($"'{arr[i]}'");
+       } 
+    }
+    Console.Write("]");
+}
+string str = "Hello!";
+char[] result = CreateCharToString(str);
+PrintArr(result);
