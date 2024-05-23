57d87435b8e991a7700ade49918c4d0284109712
Anton <augustaihenvald@gmail.com>
2024-05-23 17:48:51 +0300

    add directory

---
+++ recursion/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int Fact(int n)
+{
+    if (n == 1 || n ==0)
+    {
+       return 1;
+    }
+    Console.WriteLine(n);
+    return n * Fact(n - 1);
+}
+Console.WriteLine(Fact(5));
+
+//f11 шаг заходом
+// F10 шаг с обходом
+//F5 продолжение
